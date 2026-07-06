ef040ffe084ac5ea303d58d0fc37fdfec22a39f6
Louise <louisewang02@gmail.com>
2026-07-06 16:36:17 +1200

    Creating board.cs
    Board.cs manages the game state of the Tic-Tac-Toe grid, including storing moves, validating positions, displaying the board, and checking win or draw conditions. It is separate from game flow logic to keep the code modular and maintainable.

---
+++ Board.cs
@@ -0,0 +1,60 @@
+// Responsible for rules and states of the game this includes:
+// initialise/ reset the board (reset())
+// mark the board? (PlaceMark())
+class board
+{
+    private char[,] grid = new char[3, 3];
+
+    public board()
+    {
+        reset();
+    }
+
+    public void reset()
+    {
+        for (int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 3: c++){
+                grid[r, c] = " ";
+            }
+        }
+    }
+
+    public bool PlaceMove(int row, int col, char player)
+    {
+        if (grid[row, col] != ' ')
+            return false;
+
+        grid[row, col] = player;
+        return true;
+    }
+
+    public bool IsCellEmpty(int row, int col)
+    {
+        return grid[row, col] == ' ';
+    }
+
+    public void Display()
+    {
+        Console.WriteLine();
+        for (int r = 0; r < 3; r++)
+        {
+            Console.WriteLine(" {0} | {1} | {2} ", grid[r, 0], grid[r, 1], grid[r, 2]);
+
+            if (r < 2)
+                Console.WriteLine("---+---+---");
+        }
+        Console.WriteLine();
+    }
+
+    public bool IsFull()
+    {
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 3; c++)
+                if (grid[r, c] == ' ')
+                    return false;
+
+        return true;
+    }
+
+}
